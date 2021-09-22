8129128889b1ff462df1eb6a8abf43852856ba2e
Jerbeson Vinycius <jerbesonvinycius@gmail.com>
2021-09-22 00:09:36 -0300

    Funçao que permite captar os usuarios existentes no banco de dados

--- src/controllers/usercontroller.py
+++ src/controllers/usercontroller.py
@@ -1,10 +1,11 @@
+from sqlalchemy.future import select
 import asyncio
 
 from src.database.configuration import session
 from src.database.schemas import User
 
 # Função que permite criar usuários
-async def create_user(user: User):
+async def create_user(user: User) -> int:
   # Abrindo uma sessão no banco
   async with session() as s:
     s.add( user ) # Criando uma solicitação de adição
@@ -12,3 +13,14 @@ async def create_user(user: User):
     await s.refresh( user )
 
     return user.id
+
+# Funçao de listar todos os usuários existentes
+async def list_users() -> list:
+  # Abrindo uma sessão no banco
+  async with session() as s:
+    query = await s.execute(
+      select(User)
+    ) # Executando a consulta na tabela de usuários
+
+    # Retornando todos os usuários que foram obtidos a partir da query
+    return query.scalars().all()
